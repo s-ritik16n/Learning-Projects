d78e86f9c2465b612ce02040e86a78c7bb84c3c7
Ritik Saxena <saxenar444@gmail.com>
2018-02-24 13:10:03 +0530

    P02 - Component Props

--- P01-the-complete-react-web-developer-course-udemy/indecision-app/src/app.js
+++ P01-the-complete-react-web-developer-course-udemy/indecision-app/src/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = "Indecision";
+    const subtitle = "Put your life in the hands of a computer";
+    const options = ["One", "Two", "Three"];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle}/>
         <Action />
-        <Options />
+        <Options options={options}/>
         <AddOption />
       </div>
     );
@@ -15,10 +19,11 @@ class IndecisionApp extends React.Component {
 
 class Header extends React.Component{
   render() {
+    console.log(this.props);
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -38,7 +43,9 @@ class Options extends React.Component {
   render() {
     return (
       <ol>
-        <Option />
+          {
+            this.props.options.map((option) => <Option key={`${option}-1`} optionText={option}/>)
+          }
       </ol>
     );
   }
@@ -47,9 +54,7 @@ class Options extends React.Component {
 class Option extends React.Component {
   render (){
     return (
-      <li>
-        Option
-      </li>
+      <li key={this.props.option}>{this.props.optionText}</li>
     );
   }
 }
