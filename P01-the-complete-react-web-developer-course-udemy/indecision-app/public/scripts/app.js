2d1c43df0dd3284cc39bfd0abb76c92e32133445
Ritik Saxena <saxenar444@gmail.com>
2018-02-24 19:52:33 +0530

    P01 - Alternative setState syntax

--- P01-the-complete-react-web-developer-course-udemy/indecision-app/public/scripts/app.js
+++ P01-the-complete-react-web-developer-course-udemy/indecision-app/public/scripts/app.js
@@ -32,18 +32,29 @@ var Counter = function (_React$Component) {
     value: function handleAddOne() {
       console.log("addOne");
 
-      // this.setState((prevState) => {
-      //   return {
-      //     count : prevState.count + 1
-      //   }
-      // });
-
       // in Arrow functions with only one line of return,
       // when returned parameter is an object and if you wish not to use the keyword return,
       // enclose the arrow function in ()
       this.setState(function (prevState) {
         return { count: prevState.count + 1 };
       });
+
+      /*
+      The newer way to implement setState is -
+      this.setState({
+      count : 0
+      });
+       but, the problem with this syntax arises when we try to access the previous state of the variable, since, there is no prevState argument possible in this syntax.
+      However, the work around is to use 'this.state.variable' -
+       this.setState({
+      count : this.state.count + 1
+      });
+       But, this is still problematic.
+      Suppose, the two calls mentioned above are made sequentially. the user will expect the output to be 1, since, it is first made 0 and then incrememted by 1.
+      However, this is not the output one will get because the function setState is an asynchronous. The count might be equal to 1, or it might end up with the value count + 1. If the count gets incremented before it turns to 0, it will be retained and won't be turned to 0.
+       Now, what will happen if the stable syntax which we have used is followed and two similar calls are made to setState?
+       React will not handle the two setState events separately. it will batch the two calls into one and implement them sequentially. Thus, the syntax we have used is more stable.
+       */
     }
   }, {
     key: "handleMinusOne",
