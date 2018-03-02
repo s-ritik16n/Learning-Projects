3233c72ebf8b20c1cea3e970bb35c427a25a5f9c
ritik <saxenar444@gmail.com>
2018-03-02 13:22:48 +0530

    P02 - add component 'daashboard' using 'ng generate component dashboard'

--- P02 - Angular official/angular-tour-of-heroes/src/app/app.module.ts
+++ P02 - Angular official/angular-tour-of-heroes/src/app/app.module.ts
@@ -9,14 +9,16 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module'
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
