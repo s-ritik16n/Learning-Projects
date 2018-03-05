041a470634babb24fd87fadb01b309adbab93a01
ritik <saxenar444@gmail.com>
2018-03-05 15:01:26 +0530

    P03 - initial code

---
+++ P03 - Electron Official/myapp/main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+function createWindow() {
+
+  win = new BrowserWindow({width: 800, height: 600});
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+}
+
+app.on('ready',createWindow);
