79a9858e4633af0ac9b1339e7f410df6f8a41fd8
ritik <saxenar444@gmail.com>
2018-03-14 12:13:44 +0530

    P04 - add schema and data js files

---
+++ P04-GraphQL/scotch/src/data/posts.js
@@ -0,0 +1,128 @@
+const Posts = [{
+    'id': '0176413761b289e6d64c2c14a758c1c7',
+    'author_id': '0hy894hf0dlkfh9oinv',
+    'author': 'alonso',
+    'category': 'meteor',
+    'body': 'share the login state between multiple domains. That\'s exactly what we did. We wrapped this up into a Meteor package, which now you can also use',
+    'date': {
+      '$date': '2015-07-07T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'sharing-meteor-login-state-between-sub-domains',
+    'summary': 'In this blog we\'ll show you how we shared login state between our static web app and our Meteor app Kadira UI.',
+    'title': 'Sharing the Meteor Login State Between Subdomains'
+  },
+  {
+    'id': '03390abb5570ce03ae524397d215713b',
+    'author_id': '0hy894hf0dlkfh9oinv',
+    'author': 'alonso',
+    'category': 'product',
+    'body': 'Here is a common feedback we received from our users:\n\n\u003e Hi, I have a suggestion. It would be great if I could "dismiss" errors or mark them as resolved on my end.',
+    'date': {
+      '$date': '2015-09-01T00:00:00.000Z'
+    },
+    'slug': 'tracking-error-status-on-kadira',
+    'summary': 'Lot of users asked us to add a feature to set status for errors in the Kadira Error Manager. Now, we\'ve that functionality.',
+    'title': 'New Feature: Tracking Error Status with Kadira'
+  },
+  {
+    'id': '0be4bea0330ccb5ecf781a9f69a64bc8',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'product',
+    'body': 'script type',
+    'date': {
+      '$date': '2014-06-30T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'what-should-kadira-build-next',
+    'summary': 'We are working on the next few major feature releases for Kadira. We would like to know your preference. Pre-order the feature you would most like to see in the next major release (scheduled for August 1).',
+    'title': 'What Should Kadira Build Next?'
+  },
+  {
+    'id': '19085291c89f0d04943093c4ff16b664',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'product',
+    'body': 'Everybody hates errors',
+    'date': {
+      '$date': '2014-09-08T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'awesome-error-tracking-solution-for-meteor-apps-with-kadira',
+    'summary': 'Error tracking is so much important and goes side by side with performance issues. This is the public beta announcement of Kadira\'s error tracking solution.',
+    'title': 'Awesome Error Tracking Solution for Meteor Apps with Kadira'
+  },
+  {
+    'id': '1afff9dfb0b97b5882c72cb60844e034',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'product',
+    'body': 'You might have seen the [EventLoop Utilization](http://support.kadira.io/knowledgebase/articles/372876-event-loop-utilization) chart in our Dashboard.',
+    'date': {
+      '$date': '2014-06-12T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'tracking-cpu-usage-with-kadira',
+    'summary': 'We\'ve replaced EventLoop Utilization chart with actual CPU Usage. See why?',
+    'title': 'Tracking Meteor CPU Usage with Kadira'
+  },
+  {
+    'id': '1bd16dfab1de982317d2ba4382ec8c86',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'meteor',
+    'body': "Today is holiday for Sri Lanka. But for me, it\'s a hackday. I started playing with React and wrote few simple apps. That\'s a nice experience.",
+    'date': {
+      '$date': '2015-07-01T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'meteor-ssr-support-using-flow-router-and-react',
+    'summary': 'This is an experiment orinami did to implement Server Side Renderng(SSR) using Flow Router and React.',
+    'title': 'Meteor Server Side Rendering Support with FlowRouter and React'
+  },
+  {
+    'id': '285292901bb38be8f57dd2885c517826',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'user-story',
+    'body': "[Brent Abrahams](http://cn.linkedin.com/pub/brent-abrahams/87/372/ba1) is a mathematics teacher who has build an online curriculum management app [Standbench](http://cn.linkedin.com/pub/brent-abrahams/87/372/ba1) for his organization using Meteor. He has a [nice story](http://meteorhacks.com/meteor-in-production-a-case-study.html) with the initial stage of his applications and how he used Oplog with his application",
+    'date': {
+      '$date': '2014-05-27T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'user-stories-brent-abrahams',
+    'summary': 'Denis has been using Kadira from the initial beta release and helped us a lot on identifying issues with Kadira. This is how he is using Kadira.',
+    'title': 'How Brent is using Kadira with his development workflow'
+  },
+  {
+    'id': '2f6b59fd0b182dc6e2f0051696c70d70',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'other',
+    'body': "This is a short guide to remind you about means, histograms and percentiles in statistics. Then, we will discuss why all these matter and when we can use each of them.",
+    'date': {
+      '$date': '2015-08-24T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'mean-histogram-and-percentiles',
+    'summary': 'A short guide to means, histograms and percentiles and how we can use them in a real situation.',
+    'title': 'Understanding Mean, Histogram and Percentiles'
+  },
+  {
+    'id': '3d7a3853bf435c0f00e46e15257a94d9',
+    'author_id': '8dlx7ak38fd39dv79ad',
+    'author': 'orinami',
+    'category': 'product',
+    'body': 'Today.',
+    'date': {
+      '$date': '2015-07-20T00:00:00.000Z'
+    },
+    'layout': 'blog_post',
+    'slug': 'introducing-kadira-debug-v2',
+    'summary': 'Today, we are introducing a new version of Kadira Debug. It comes with many UI improvements and support for CPU profiling.',
+    'title': 'Introducing Kadira Debug, Version 2'
+  }
+];
+
+module.exports = Posts;
--- P04-GraphQL/scotch/src/schema.js
+++ P04-GraphQL/scotch/src/schema.js
@@ -9,3 +9,50 @@ let  {
   GraphQLSchema,
 
 } = require('graphql');
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This repersent an author",
+  fields: () => ({
+    id: {type: new GraphQLNonNull(GraphQLString)},
+    name: {type: new GraphQLNonNull(GraphQLString)},
+    twitterHandle: {type: GraphQLString}
+  })
+});
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => {
+    id: {type: new GraphQLNonNull(GraphQLString)},
+    title: {type: new GraphQLNonNull(GraphQLString)},
+    body: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve: function(post) {
+        return _.find(Authors, a=>a.id == post.author_id);
+      }
+    }
+  }
+})
+
+const BlogQueryType = new GraphQLObjectType({
+  name: "BLogAppSchema",
+  description: "Blog Application",
+  fields: () => {
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "Lit of all authors",
+      resolve: function() {
+        return Authors
+      }
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      description: "List of all Posts",
+      resolve: function() {
+        return Posts
+      }
+    }
+  }
+});
